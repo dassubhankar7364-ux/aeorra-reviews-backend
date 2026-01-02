2e566c64fea44803182b3ce8bf8338123be61898
dassubhankar7364-ux <dassubhankar7364@gmail.com>
2026-01-02 22:17:39 +0530

    Fix CORS for admin panel

--- server.js
+++ server.js
@@ -8,7 +8,11 @@ const reviewRoutes = require("./routes/reviewRoutes");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 app.use(express.json());
 
 app.use("/api/reviews", reviewRoutes);
